32148ce23aa0ec0251a9a3f12885c82d84205243
sirara1 <sarra.benaissa@esen.tn>
2025-02-28 01:17:22 +0100

    User

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controllers/userController');
+const upload = require('../middlewares/uploadFile');
+
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json('respond with a resource');
-});
+router.post('/addUserTechnicien',userController.addUserTechnicien); 
+router.post('/addUserAdmin',userController.addUserAdmin); 
+router.get('/getAllUsers',userController.getAllUsers); 
+router.get('/getUserById/:id',userController.getUserById); 
+router.delete('/deleteUserById/:id',userController.deleteUserById); 
+router.put('/updateuserById/:id',userController.updateuserById); 
+router.get('/searchUserByUsername',userController.searchUserByUsername); 
+router.get('/getAllTechnicien',userController.getAllTechnicien); 
+router.get('/getAllAdmin',userController.getAllAdmin); 
 
 module.exports = router;
+
