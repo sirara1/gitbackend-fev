4ef7da9dfb796f8fe518541b20e33a0976137da7
sirara1 <sarra.benaissa@esen.tn>
2025-02-28 01:16:52 +0100

    User

---
+++ controllers/usercontroller.js
@@ -0,0 +1,129 @@
+const userModel = require('../models/userSchema');
+module.exports.addUserTechnicien = async (req,res) => {
+    try {
+        const {username , email , password ,} = req.body;
+        const roleTechnicien = 'Technicien'
+        // if (!checkIfUserExists) {
+        //     throw new Error("User not found");
+        //   }
+        const user = await userModel.create({
+            username,email ,password,role :roleTechnicien 
+        })
+        // const user = new userModel({name,age,address,moy});
+        //   const adduser = await user.save();
+        res.status(200).json({user});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }   } 
+
+    module.exports.addUserAdmin = async (req,res) => {
+        try {
+            const {username , email , password ,} = req.body;
+            const roleAdmin = 'admin'
+            // if (!checkIfUserExists) {
+            //     throw new Error("User not found");
+            //   }
+            const user = await userModel.create({
+                username,email ,password,role :roleAdmin
+            })
+            // const user = new userModel({name,age,address,moy});
+            //   const adduser = await user.save();
+            res.status(200).json({user});
+        } catch (error) {
+            res.status(500).json({message: error.message});
+        }   } 
+
+
+        module.exports.getAllUsers= async (req,res) => {
+            try {
+                const userListe = await userModel.find()
+                res.status(200).json({userListe});
+            } catch (error) {
+                res.status(500).json({message: error.message});
+            }
+        }
+        module.exports.getUserById= async (req,res) => {
+            try {
+                //const id = req.params.id
+                const {id} = req.params
+                //console.log(req.params.id)
+                const user = await userModel.findById(id)
+        
+                res.status(200).json({user});
+            } catch (error) {
+                res.status(500).json({message: error.message});
+            }
+        }
+
+        module.exports.deleteUserById= async (req,res) => {
+            try {
+                const {id} = req.params
+        
+                const checkIfUserExists = await userModel.findById(id);
+                if (!checkIfUserExists) {
+                  throw new Error("User not found");
+                }
+                await userModel.findByIdAndDelete(id)
+        
+                res.status(200).json("deleted");
+            } catch (error) {
+                res.status(500).json({message: error.message});
+            }
+        }
+
+        module.exports.updateuserById = async (req, res) => {
+try {
+    const {id} = req.params
+    const {email , username} = req.body;
+
+    await userModel.findByIdAndUpdate(id,{$set : {email , username }})
+    const updated = await userModel.findById(id)
+
+    res.status(200).json({updated})
+} catch (error) {
+    res.status(500).json({message: error.message});
+}
+}
+module.exports.searchUserByUsername = async (req, res) => {
+    try {
+
+        const { username } = req.query
+        if(!username){
+            throw new Error("Veuillez fournir un nom pour la recherche.");
+        }
+
+        const userListe = await userModel.find({
+            username: {$regex: username , $options: "i"}
+        })
+
+        if (!userListe) {
+            throw new Error("User not found");
+          }
+          const count = userListe.length
+        res.status(200).json({userListe,count})
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+    }
+
+    module.exports.getAllTechnicien= async (req,res) => {
+        try {
+
+            const userListe = await userModel.find({role : "Technicien"})
+    
+            res.status(200).json({userListe});
+        } catch (error) {
+            res.status(500).json({message: error.message});
+        }
+    }
+
+    module.exports.getAllAdmin= async (req,res) => {
+        try {
+
+            const userListe = await userModel.find({role : "admin"})
+    
+            res.status(200).json({userListe});
+        } catch (error) {
+            res.status(500).json({message: error.message});
+        }
+    }
---
+++ models/userSchema.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 5,
+      match: [
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        "Le mot de passe doit contenir au moins 5 caractères, une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial.",
+      ],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "Technicien", ],
+    },
+    user_image: { type: String, require: false, default: "utilisateur.png" },
+    age: {type : Number },
+    //count: {type : Number, default:'0'},
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt();
+    const user = this;
+    user.password = await bcrypt.hash(user.password, salt);
+    //user.etat = false ;
+    //user.count = user.count + 1;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.post("save", async function (req, res, next) {
+  console.log("connected");
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
